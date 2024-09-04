3b16850125bbc35054334c0d61a028cda7c6926a
Lindsay Kuperman <LMK22E@FSU.EDU>
2024-09-03 23:24:29 -0400

    It works with a mouse!

--- sketch.js
+++ sketch.js
@@ -3,5 +3,13 @@ function setup() {
 }
 
 function draw() {
-  background(125);
+  //sky blue background
+  background(135, 206, 235);
+  // sun in top-right corner
+  fill("yellow"); //yellow
+
+  stroke("orange"); //orange outline
+
+  strokeweight (20); //large outline
+  cirlce(550, 50, 100)
 }
